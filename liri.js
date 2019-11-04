c1b31e39dbc8a396a3b5196912ae5a052a73819d
ctslone <ctslone27@gmail.com>
2019-11-03 22:14:56 -0500

    adding more to liri js.started on bands in town command. got a response back for a band but need to decode the response and navigate through all objects in the response (loop through). got the OMDB command working properlygit status

--- liri.js
+++ liri.js
@@ -1,2 +1,56 @@
-require("dotenv").config();
-var keys = require("./keys.js");
+// require(".env").config();
+var axios = require("axios");
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2]
+
+switch(command) {
+    case 'concert-this':
+        // some stuff happens
+        var artist = process.argv[3];
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(queryUrl).then(function(response) {
+
+            // If the request with axios is successful
+            // console.log(response);
+            for (key in response.data) {
+                if (response.data.hasOwnProperty(key)) {
+                    console.log(key + "->" + response.data[key])
+                }
+            }
+
+            // console.log(response.data)
+            
+            // Then log the Release Year for the movie
+            // console.log("Release year: " + response.data.Year)
+            })
+        // some stuff happens
+        break;
+    case 'spotify-this-song':
+        // some stuff happens
+        // some stuff happens
+        break;
+    case 'movie-this':
+            var movie = process.argv[3];
+            var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+            axios.get(queryUrl).then(function(response) {
+                // If the request with axios is successful
+                // console.log(response.data);
+                console.log("Movie title: " + response.data.Title);
+                console.log("Year released: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.Ratings[0]["Value"]);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1]["Value"]);
+                console.log("Country of Production: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+                })
+        break;
+    case 'do-what-it-says':
+        // some stuff happens
+        // some stuff happens
+        break;
+    
+
+}
